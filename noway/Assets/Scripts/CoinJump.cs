8e976c172b87de868b48f0a7e700692ac385bb9e
Nickolaus D. Saint <nick@windwardproductions.org>
2018-07-07 18:22:23 -0700

    Added very basic scene to test gameplay

---
+++ noway/Assets/Scripts/CoinJump.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinJump : MonoBehaviour {
+
+	public float jumpForce = 1000f;
+	public AudioSource chipCollide;
+
+	Rigidbody2D rigidbody;
+	int chapsLayer;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = gameObject.GetComponent<Rigidbody2D>();
+		chapsLayer = LayerMask.NameToLayer("Chaps");
+
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {	
+		if (col.gameObject.layer == chapsLayer) {
+			rigidbody.AddForce(new Vector2(0f, jumpForce));
+			if (chipCollide) {
+				chipCollide.Play(0);
+			}
+			Destroy(col.gameObject);
+		}
+    }
+}
